eec1f63fae03abd15c4a95859f6ae1680eee9ecd
Cong-Thuan Do <congthuan274@gmail.com>
2021-11-21 13:29:31 +0100

    design(home): main design of homepage done

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react'
-import { View, Text, StyleSheet, Button, SafeAreaView, FlatList, ActivityIndicator } from 'react-native'
+import { View, Text, StyleSheet, Button, SafeAreaView, FlatList, ActivityIndicator, Image, TouchableOpacity } from 'react-native'
+import { Card, Avatar, ButtonGroup, Chip } from 'react-native-elements'
 
 const HomeScreen = ({ navigation }) => {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
+  const topButtons = ['Programmation', 'Artists']
 
   // Fetch data method
   const getArtists = async () => {
@@ -23,18 +25,60 @@ const HomeScreen = ({ navigation }) => {
   }, []);
 
   return (
-    <View style={{ flex: 1, padding: 24 }}>
+    <View style={styles.container}>
+
       {isLoading ? <ActivityIndicator /> : (
-        <FlatList
-          data={data}
-          keyExtractor={(item,  index) =>  item.artist_id}
-          renderItem={({ item }) => (
-            <Text>{item.artist_name}, {item.artist_description}</Text>
-          )}
-        />
+        <View>
+          {/* Top Buttons */}
+          <ButtonGroup
+            // onPress={this.updateIndex} 
+            // selectedIndex={selectedIndex} 
+            buttons={topButtons}
+            containerStyle={{ height: 40 }} />
+
+          {/* Date chips */}
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <Chip title="Tous" />
+            <Chip title="Jeu 25 Août" type="outline" />
+            <Chip title="Ven 26 Août" type="outline" />
+            <Chip title="Sam 27 Août" type="outline" />
+          </View>
+
+          {/* List of artists */}
+          <FlatList
+            data={data}
+            keyExtractor={(item, index) => item.artist_id}
+            renderItem={({ item }) => (
+
+              <TouchableOpacity>
+                <Card containerStyle={{ marginBottom: -10 }}>
+                  <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    <Avatar rounded source={{ uri: 'https://i.pravatar.cc/150', }} />
+                    <View style={{ justifyContent: 'center', marginLeft: 20 }}>
+                      <Text style={{ fontWeight: 'bold', marginBottom: 5 }}>{item.artist_name}</Text>
+                      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <Text style={{ marginRight: 20 }}>Scène n°{item.stage_id}</Text>
+                        <Text>{item.artist_concert_time}</Text>
+                      </View>
+                    </View>
+                  </View>
+                </Card>
+              </TouchableOpacity>
+
+            )}
+          />
+        </View>
+
       )}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24
+  },
+});
 
 export default HomeScreen
